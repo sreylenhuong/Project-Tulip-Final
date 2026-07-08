d0b8a76d50b14a326016e829a6196c8bf07f36f1
sreylenhuong <huongsreylen@gmail.com>
2026-07-08 21:05:05 +1200

    low sound

--- js/audio.js
+++ js/audio.js
@@ -7,7 +7,7 @@
 function initAudio() {
   const FADE_STEP_MS = 80;
   const MUSIC_MAX_VOLUME = 0.50;
-  const CARD_SOUND_MAX_MS = 80;
+  const CARD_SOUND_MAX_MS = 1;
 
   const sounds = {
     seal: new Audio('audio/seal.mp3'),
@@ -27,7 +27,7 @@ function initAudio() {
   sounds.music.loop = true;
 
   sounds.seal.volume = 0.42;
-  sounds.folderOpen.volume = 0.015;
+  sounds.folderOpen.volume = 0.005;
   sounds.cardSlide.volume = 0.18;
   sounds.music.volume = 0;
 
